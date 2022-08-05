b45d89b573a795415e78bcbb3d24f92e0061e4a0
legion <islomnizom@gmail.com>
2022-08-05 07:56:44 +0300

    6.1. Классический поиск перебором. [#33489]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -2,13 +2,13 @@ package ru.job4j.array;
 
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
-        int rst = -1;
+        int rs = -1;
         for (int index = 0; index < data.length; index++) {
             if (data[index] == el) {
-                rst = index;
+                rs = index;
                 break;
             }
         }
-        return rst;
+        return rs;
     }
 }
